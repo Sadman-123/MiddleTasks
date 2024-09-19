6f54820f8957fb2a3a39a9146da7758b9d5f38bc
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-19 19:04:47 +0600

    login works and gives jwt token

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app=require('express')();
 const cors=require('cors');
 const bdy=require('body-parser');
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 app.use(cors());
 app.use(bdy.json());
 app.use(bdy.urlencoded({extended:true}));
@@ -33,22 +34,26 @@ app.listen(process.env.port,()=>{
     console.log('listening')
 })
 
-app.post('/login',async (req,res)=>{
-    const {username,password}=req.body;
-    const user= await db.findOne({
-        username:username
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await db.findOne({
+        username: username
     });
-    if(user)
-    {
-        bcrypt.compare(password, user.password, function(err, result) {
-            if(result){
-                res.status(200).send('Login Successful')
+    if (user) {
+        bcrypt.compare(password, user.password, async function (err, result) {
+            if (result) {
+                const token = await jwt.sign({
+                    id: user._id,
+                    username: user.username
+                }, process.env.secret, { expiresIn: '2h' });
+                return res.status(200).json({
+                    msg: "Login Successful",
+                    token: token,
+                });
             }
-            res.status(404).send('Password not matched')
+            return res.status(404).send('Password not matched');
         });
+    } else {
+        res.status(404).send('No User Found');
     }
-    else{
-        res.status(404).send('No User Found')
-    }
-    
-})
+});

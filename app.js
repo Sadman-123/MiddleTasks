357402f59828cc3c8c41d1d99a693997d53d74ac
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-09-19 18:45:13 +0600

    already exist

--- app.js
+++ app.js
@@ -1,77 +1,33 @@
 require('dotenv').config();
+const db=require('./db/userdb')
 const app=require('express')();
-const bdy=require('body-parser');
 const cors=require('cors');
-const jwt=require('jsonwebtoken');
+const bdy=require('body-parser');
+const bcrypt=require('bcrypt');
+app.use(cors());
 app.use(bdy.json());
 app.use(bdy.urlencoded({extended:true}));
-app.use(cors());
-const bcrypt=require('bcrypt');
-const userdb=require('./db/userdb');
-const middleman=require('./middleware/auth')
-app.post('/registration',async (req,res)=>{
-    const uname=req.body.username;
-    const pass=req.body.password;
-    const user=await userdb.findOne({username:uname});
-   try{
-        if(user)
-        {
-           res.status(401).send("user Already exists") 
-        }
-        else{
-            bcrypt.hash(pass, 10, async function(err, hash) {
-                const newuser=new userdb({
-                    username:uname,
-                    password:hash,
-                    createdtime:new Date().toUTCString()
-                });
-                await newuser.save()
-                .then(()=>{
-                    res.status(200).json({
-                        "create":true,
-                        "username":uname,
-                        "password":hash
-                    })
-                })
-                .catch(err=>res.status(401).send('Error while creating user'))
-            });
-        }
-   }
-   catch(e)
-   {
-    console.log(e)
-   }   
-})
-app.post('/login',async(req,res)=>{
-    const uname=req.body.username;
-    const pass=req.body.password;
-    const user=await userdb.findOne({username:uname});
-    if(user){
-        bcrypt.compare(pass, user.password, function(err, result) {
-            if(result)
-                {
-                    const token=jwt.sign({id:user._id,username:user.username},process.env.secret,{expiresIn:'2h'})
-                    res.status(200).json({
-                        login:true,
-                        token:token
-                    })
-                }
-                else
-                    {
-                        res.status(401).send("Password Dont Match")
-                    } 
-        });
+app.post('/register',async(req,res)=>{
+    const {username,password}=req.body;
+    const user= await db.findOne({
+        username:username
+    });
+    if(user)
+    {
+        res.status(400).send('Already exist')
     }
     else{
-        res.status(401).send("User Not Exists")
+        const newuser=new db({
+            username:username,
+            password:password,
+            createdtime:new Date().toUTCString()
+        });
+        await newuser.save()
+        .then(()=>res.status(200).send('user created'))
+        .catch(e=>console.log(e))
+
     }
-
-})
-app.get('/userdata',middleman,(req,res)=>{
-    res.send("Hello "+req.username)
-
 })
 app.listen(process.env.port,()=>{
     console.log('listening')
 })
-
